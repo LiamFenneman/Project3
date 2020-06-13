dcf3ed7504c55d68b3037187d0f894f66f33cfd6
LiamFenneman <liamfennemanbusiness@gmail.com>
2020-06-13 20:58:19 +1200

    Added further comments
    NOTE: server3.cpp must be recompiled for last commit fixes to be applied.

--- AVC_robot/robot.cpp
+++ AVC_robot/robot.cpp
@@ -1,42 +1,34 @@
 #include "robot.hpp"
 
-// returns 1 when a pixel is white and 0 otherwise
-int isWhite(int row, int col) {
-	int pix = get_pixel(cameraView, row, col, 3);
-	int isWhite;
-	
-	if (pix > 250)	{ isWhite = 1; }
-	else 			{ isWhite = 0; }
-	
-	return isWhite;
+// find the first column that contains a white pixel
+// 	-	using the middle of the camera view
+int findFirstWhiteCol() {
+	int row = cameraView.height/2; // middle row
+	// loop through all the pixels on the middle row
+	for (int col = 0; col < cameraView.width; col++) {
+		int pix = get_pixel(cameraView, row, col, 3); // get the pixel
+		
+		// if the pixel is white -> return the column no.
+		if (pix > 250) { return col; }
+	}
 }
 
 // checks to see if the flag is present on the camera
 //  -	used to stop the robot upon reaching the finish line
 int isFlag() {
 	int count = 0;
+	// loop through all pixels (all rows / columns) in the camera view
 	for (int row = 0; row < cameraView.height; row++) {
 		for (int col = 0; col < cameraView.width; col++) {
-			int pix = get_pixel(cameraView, row, col, 3);
+			int pix = get_pixel(cameraView, row, col, 3); // get the pixel
 			
+			// if the pixel is black -> add one to the counter
 			if (pix < 10) { count++; }
 		}
 	}
 	
+	// if there are more than 1000 black pixels we can safely assume the flag is present
 	return count > 1000;
-}
-
-// find the first column that contains a white pixel
-// 	-	using the middle of the camera view
-int findFirstWhiteCol() {
-	int col = 0;
-	while(1) {
-		if (col >= cameraView.width) { break; }
-		if (isWhite(cameraView.height/2, col)) {
-			return col;
-		}
-		col++;
-	}
 }
 
 
@@ -56,13 +48,14 @@ int main() {
 	double ratio = 1.5;
 	
 	while(1) {
-		int midCol = cameraView.width/2;
 		takePicture();
 		
-		int firstCol = findFirstWhiteCol();
-		int diff = firstCol - midCol;
-		int dir = (diff > 0);
-		
+		int midCol = cameraView.width/2;	// middle column
+		int firstCol = findFirstWhiteCol(); // first column that is white in the middle row
+		int diff = firstCol - midCol;		// the distance between the middle column and first white pixel
+		int dir = (diff > 0);				// the direction which the robot should move to correct itself
+											//  - move right when the line is to the right of the robot
+		// move the robot
 		if (isFlag()) { // stop on flag
 			vLeft = 0;
 			vRight = 0;
